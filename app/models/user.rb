8232d886f4f6fd1a2fc534ce14c3c20c2607637b
scarecrow182000 <eric_d_epperson@hotmail.com>
2014-09-26 13:19:08 -0500

    No devise registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable  :rememberable and :omniauthable
-  devise :database_authenticatable, :recoverable, :trackable, :validatable, :registerable
+  # :confirmable, :lockable, :timeoutable  :rememberable and :omniauthable, :registerable
+  devise :database_authenticatable, :recoverable, :trackable, :validatable
 end
