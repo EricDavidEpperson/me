b1169542162607faa1981f86eeec4bcfacaa702e
scarecrow182000 <eric_d_epperson@hotmail.com>
2014-09-26 12:39:38 -0500

    Deploy blog to Heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable :registerable :rememberable and :omniauthable
-  devise :database_authenticatable, :recoverable, :trackable, :validatable
+  # :confirmable, :lockable, :timeoutable  :rememberable and :omniauthable
+  devise :database_authenticatable, :recoverable, :trackable, :validatable, :registerable
 end
