7175f2ddb5e77a0af96da4b0fe788d17a6c3392a
scarecrow182000 <eric_d_epperson@hotmail.com>
2014-09-26 16:23:01 -0500

    Tweaks

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :body, presence: true
+
+  # scope :by_created_at, order(created_at: :asc)
+  default_scope { order('created_at DESC') }
 end
