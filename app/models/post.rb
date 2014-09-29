401befc413c3de8fd454102f90315ea066b72232
scarecrow182000 <eric_d_epperson@hotmail.com>
2014-09-29 05:03:42 -0500

    Div order and blog changes

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,5 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :body, presence: true
 
-  # scope :by_created_at, order(created_at: :asc)
   default_scope { order('created_at DESC') }
 end
