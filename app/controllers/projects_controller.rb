9756de7ebf2abd8463cd5776d76b5eddf60007e4
scarecrow182000 <eric_d_epperson@hotmail.com>
2014-09-30 14:28:24 -0500

    Project editing

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,12 @@
 class ProjectsController < ApplicationController
+  before_action :authenticate_user!, :except => [:index]
+
   def index
     @projects = Project.all
+  end
+
+  def show
+    @project = Project.find(params[:id])
   end
 
   def new
